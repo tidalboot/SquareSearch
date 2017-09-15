f8134ad0f144ef6455ec077a0d339c8d113bcf2f
tidalboot <nickjones@Nicks-MacBook-Pro-3.local>
2017-09-15 13:58:49 +0100

    Set up a search bar and a helper label on the search screen and introduced a new Critic class which will be used to handle all network requests

--- SquareSearch/Controllers/Search.swift
+++ SquareSearch/Controllers/Search.swift
@@ -6,10 +6,17 @@ import Foundation
 import UIKit
 
 //Going for Search rather than something like "SearchController" as I think the inheritance from UIViewController provides this information itself
-class Search: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class Search: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate {
+    
+    // MARK: - Constant Strings 🎻
+    // If the app were to grow there could be an argument to move these out to a seperate class to make localisation easier; For now they're fine
+    private let findingVenues = "Finding venues! \n🕵️🕵️‍♀️"
+    private let noVenues = "Sorry, we couldn't find any venues for you\n😭"
+    private let whatAreYouLookingFor = "Where would you like to find the best venues?\n🦄"
     
     // MARK: - Local Properties 💻
     private var searchResults: UICollectionView!
+    private var helperLabel: UILabel!
     private var venues = [Venue]()
     
     // MARK: - Collection View Delegate Methods 📦
@@ -69,6 +76,24 @@ class Search: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
         return searchItem
     }
     
+    // MARK: - Search Bar Delegate Methods 🔎
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        if let searchBarText = searchBar.text {
+            getVenues(withLocation: searchBarText)
+        }
+    }
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.searchResults.alpha = 0
+        }) { (_) in
+            self.helperLabel.transitionText(withString: self.whatAreYouLookingFor)
+        }
+        
+        return true
+    }
+    
     // MARK: - UIView Delegate Methods 👑
     //Whilst we could move a lot of this out to builder methods or elsewhere since it's only being used it feels like a waste; Better to keep it here for now
     //If we had tests around any of this then we'd definitely need to move it out to allow it to be testable. But initially I'm only interested in ensuring that our data parsing is correct and working
@@ -86,13 +111,38 @@ class Search: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
         layout.minimumLineSpacing = 0
         //-----
         
+        //Helper Label
+        helperLabel = UILabel(frame: CGRect(
+            x: view.frame.size.width * 0.1,
+            y: (view.frame.size.height * 0.4) - 125,
+            width: view.frame.size.width * 0.8,
+            height: 250
+            )
+        )
+        helperLabel.numberOfLines = 0
+        helperLabel.textColor = .darkGray
+        helperLabel.textAlignment = .center
+        helperLabel.font = UIFont.systemFont(ofSize: 40)
+        view.addSubview(helperLabel)
+        //-----
+        
+        //Search Bar
+        let searchBar = UISearchBar(frame: CGRect(
+            x: 0,
+            y: 20,
+            width: view.frame.size.width,
+            height: 50)
+        )
+        searchBar.delegate = self
+        view.addSubview(searchBar)
+        //-----
         
         //Gallery Collection View
         let gallerySize = CGRect(
             x: 0,
-            y: 0,
+            y: searchBar.frame.origin.y + searchBar.frame.size.height,
             width: view.frame.size.width,
-            height: view.frame.size.height
+            height: view.frame.size.height - (searchBar.frame.origin.y + searchBar.frame.size.height)
         )
         searchResults = UICollectionView(frame: gallerySize, collectionViewLayout: layout)
         searchResults.contentInset = .zero
@@ -105,24 +155,37 @@ class Search: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
         //-----
         
         view.addSubview(searchResults)
-        
-        getVenues()
+        searchBar.becomeFirstResponder()
     }
     
     // MARK: - Custom Methods 🔮
     private func showNewVenues() {
+        helperLabel.text = ""
         searchResults.reloadData()
         searchResults.setContentOffset(.zero, animated: false)
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.searchResults.alpha = 1
+        })
     }
     
-    private func getVenues() {
-        if let mockedVenuesDataPath = Bundle.main.url(forResource: "ColchesterVenues", withExtension: "json") {
-            do {
-                let rawVenuesData = try Data(contentsOf: mockedVenuesDataPath)
-                venues = Venue.parseVenues(fromRawData: rawVenuesData)
-                showNewVenues()
-            } catch {
-                return
+    private func getVenues(withLocation location: String = "") {
+        
+        helperLabel.transitionText(withString: findingVenues) {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.searchResults.alpha = 0
+            }) { (_) in
+                Critic().findVenues(byLocation: location) { (venuesReturned) in
+                    DispatchQueue.main.async {
+                        if (venuesReturned.isEmpty) {
+                            self.helperLabel.transitionText(withString: self.noVenues)
+                            return
+                        } else {
+                            self.venues = venuesReturned
+                            self.showNewVenues()
+                        }
+                    }
+                }
             }
         }
     }
---
+++ SquareSearch/Handlers/Critic.swift
@@ -0,0 +1,56 @@
+
+//  Critic
+//  Copyright © 2017 NickJones. All rights reserved.
+
+import Foundation
+
+//Critic handles all venue network requests; Any additional network requests that need to be added should be done so in here
+class Critic {
+    // MARK: - Local Properties 💻
+    //The base endpoint shouldn't be modifiable outside of this class
+    private let baseEndpoint = "https://api.foursquare.com/v2"
+    
+    private let clientID = "R3A1A5YH3D5BA3U3Y3QLDI14CYZKDSUSLSIIBIX0VOV12PFH"
+    private let clientSecret = "LAFF1OQNFA5GLBZVBKG4SA1Y3FXJYGODL512RMZPGSS2BTS4"
+    private let version = "20170901"
+    
+    private func authenticatedEndpoint(withQuery query: String) -> String {
+        return "\(baseEndpoint)/\(query)&client_id=\(clientID)&&client_secret=\(clientSecret)&v=\(version)"
+    }
+    
+    // MARK: - Custom Methods 🔮
+    //For safety we'll always pass a typed array of Venues through to our completion block; The Venue parser also returns a typed array of venues no matter what to fit this
+    func findVenues(byLocation location: String, withCompletionHandler completionAction: @escaping ([Venue]) -> Void) {
+        
+        //First we set up the endpoint that we want to hit
+        let endpointToUse = authenticatedEndpoint(withQuery: "/venues/search?near=\(location)&limit=5")
+        
+        var venues = [Venue]()
+        
+        //Then we ensure that our endpoint is still a valid URL
+        guard let endpointURL = URL(string: endpointToUse) else {
+            completionAction(venues)
+            return
+        }
+        
+        //Create our data task
+        let task = URLSession.shared.dataTask(with: endpointURL) { (rawData, rawResponse, rawError) in
+            
+            if (rawError != nil || rawData == nil) {
+                completionAction(venues)
+                return
+            }
+            
+            guard let data = rawData else {
+                completionAction(venues)
+                return
+            }
+            
+            venues = Venue.parseVenues(fromRawData: data)
+            completionAction(venues)
+        }
+        //And kick it off
+        task.resume()
+    }
+}
+
